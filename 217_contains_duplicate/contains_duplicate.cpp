0de384b39a75bada0e4197e7dc444279398083ef
Diogo Carrola <diogo.carrola@gmail.com>
2025-06-04 23:21:50 +0100

    Replace contains() with count() for unordered_set compatibility

--- 217_contains_duplicate/contains_duplicate.cpp
+++ 217_contains_duplicate/contains_duplicate.cpp
@@ -1,11 +1,12 @@
+#include <vector>
+#include <unordered_set>
+
 class Solution {
-    public:
-    #include <unordered_set>
-    
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> seen;
+public:
+    bool containsDuplicate(std::vector<int>& nums) {
+        std::unordered_set<int> seen;
         for (int num : nums) {
-            if (seen.contains(num)) return true;
+            if (seen.count(num)) return true;
             seen.insert(num);
         }
         return false;

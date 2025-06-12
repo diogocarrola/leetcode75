a2520661d4dd0d6facaac73866bf9b8c36f92c55
Diogo Carrola <diogo.carrola@gmail.com>
2025-06-12 12:54:09 +0100

    Add solution for Search in Rotated Sorted Array in C (One-Pass)

---
+++ 033_search_in_rotated_sorted_array/search_in_rotated_sorted_array.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int search(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        // Check if the left side is sorted
+        if (nums[left] <= nums[mid]) {
+            // Target is in the left half
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            // Right side must be sorted
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+}
